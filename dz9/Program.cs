9784c5f87e8363cc0c2c3a29d178b38b441fdc9a
max1266 <118952053+max1266@users.noreply.github.com>
2023-01-05 20:52:01 +0700

    Задача 64
    Задача 64

---
+++ dz9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("введите m число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите n число");
+int n = int.Parse(Console.ReadLine());
+Console.Write(naturalnumbers(m,n));
+string naturalnumbers(int m, int n)
+{
+    if(n==m)
+    {
+       Console.WriteLine(n);
+        return n.ToString();
+    }
+    string s = naturalnumbers(m + 1, n) + " " + m.ToString();
+    return(s);
+}
