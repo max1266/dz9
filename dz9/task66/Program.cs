468bfeda079b4d846669bb3d7c2be937e1fbce08
Maxim Shmidt <history1266@mail.ru>
2023-01-05 20:25:58 +0700

    Задача 66

---
+++ dz9/task66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("введите m число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите n число");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"Сумма чисел в этом диапазоне : {naturalnumbers(m,n)}");
+int naturalnumbers(int m, int n)
+{
+    if(m == n)
+    {
+        Console.WriteLine("Конец");
+        return n;
+    }
+    int summ = m  + naturalnumbers(m + 1 , n);
+    return summ;
+}
